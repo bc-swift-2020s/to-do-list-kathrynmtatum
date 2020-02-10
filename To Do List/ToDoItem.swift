b904e22b351014c6caa66c89215244501fa77132
Kathryn Tatum <tatumka@bc.edu>
2020-02-09 22:06:34 -0500

    5.6 Structs

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Kathryn Tatum on 2/9/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
+
+
